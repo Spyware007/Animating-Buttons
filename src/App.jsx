15f1f50836cf4d0dba32280ebedd396da91f2856
Sushant Gangwar <sushant.gangwar.work@gmail.com>
2022-10-15 19:23:56 +0530

    Added Dark Mode (#15)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { Main, Navbar, Socials } from "./components";
 
 const App = () => {
+
+  const [toggleMode, setToggleMode] = useState(false)
+
   return (
-    <>
-      <Navbar />
-      <Socials />
+    <div className={`app ${toggleMode ? "dark" : "light"}`}>
+      <Navbar modeToggle={toggleMode} modeToggleFunc={setToggleMode} />
+      <Socials modeToggle={toggleMode} />
       <Main />
-    </>
+    </div>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import classes from "./Navbar.module.css";
 import github from "../../assets/github.png";
 import gdsc from "../../assets/gdsc.png";
+import moon from "../../assets/moon.png"
+import sun from "../../assets/sun.png"
 
-const Navbar = () => {
+const Navbar = ({ modeToggle, modeToggleFunc }) => {
   return (
     <>
       <nav className={classes.navbar}>
@@ -15,6 +17,9 @@ const Navbar = () => {
         >
           <img className={classes.logo} src={gdsc} alt="gdsc_logo" />
         </a>
+        <button className={`${classes.mode_toggle} ${modeToggle ? classes.dark_mode : classes.light_mode}`} onClick={()=>modeToggleFunc(!modeToggle)}>
+          <img src={modeToggle ? sun : moon} alt="" />
+        </button>
         <a
           className={classes.link}
           href="https://github.com/Spyware007/Animating-Buttons"
