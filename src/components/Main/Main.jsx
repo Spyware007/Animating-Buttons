0edf630e6fd36f1305f35e5f1c2183f18d16112f
Ayush Shukla <88582186+123shuklaayush@users.noreply.github.com>
2022-10-16 12:05:09 +0530

    updated button to the center (#23)
    * added new button
    
    * Delete index.html
    
    * Delete style.css
    
    * added new effect
    
    * updated button to center
    
    * added button to center
    
    * button added to center
    
    * decreased font-size

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -33,6 +33,11 @@ const Main = () => {
         ></iframe>
         <iframe
           className={classes.container}
+          title="123shuklaayush"
+          src="Buttons/123shuklaayush_1/index.html"
+        ></iframe>
+        <iframe
+          className={classes.container}
           title="Arjun-Ingole"
           src="Buttons/Arjun-Ingole/index.html"
         ></iframe>
