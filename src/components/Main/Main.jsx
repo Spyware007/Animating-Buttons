e449e4be40371e6ee30352e13ffbfb8a39e6e9aa
DipHire <diphire111@gmail.com>
2022-10-17 21:23:40 +0530

    apparently i did something…

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -219,6 +219,11 @@ const Main = () => {
           title="NMN03_1"
           src="Buttons/NMN03_1/index.html"
         ></iframe>
+         <iframe
+          className={classes.container}
+          title="DipHire"
+          src="Buttons/DipHire/index.html"
+        ></iframe>
 
       </div>
     </>
