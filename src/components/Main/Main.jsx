4c0fe7fcf50665886d97cb8c82cdb2ab98a0ff24
Nevish-302m <shiven302@gmail.com>
2022-10-18 16:23:38 +0530

    download-function-added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,241 +1,316 @@
 import React from "react";
 import classes from "./Main.module.css";
+import download from "../Functions/Download"
 
 const Main = () => {
   return (
     <>
       <h1 className={classes.text}>Explore the Buttons by our Contributors.</h1>
       <div className={classes.btns_container}>
-        <iframe
+        <div> <iframe
           className={classes.container}
           title="AshutoshRajGupta"
           src="Buttons/AshutoshRajGupta/index.html"
-        ></iframe>
-         <iframe
-          className={classes.container}
-          title="shubham-2410"
-          src="Buttons/shubham-2410/index.html"
-        ></iframe>
-        <iframe
-          className={classes.container}
-          title="SaurabhBarde-007"
-          src="Buttons/SaurabhBarde-007_2/index.html"
-        ></iframe>
-         <iframe
-          className={classes.container}
-          title="onkar2921"
-          src="Buttons/onkar2921/index.html"
-        ></iframe>
-        <iframe
+        ></iframe> 
+        <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`AshutoshRajGupta`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="SaurabhBarde-007"
           src="Buttons/SaurabhBarde-007_1/index.html"
-        ></iframe>
+        ></iframe>  
+        <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`SaurabhBarde-007_1`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
 
-        <iframe
+        <div> <iframe
           className={classes.container}
           title="ganraj21"
           src="Buttons/ganraj21/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`ganraj21`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="ganraj21_1"
           src="Buttons/ganraj21_1/index.html"
-        ></iframe>
-
-
-        <iframe
-          className={classes.container}
-          title="ganraj21_2"
-          src="Buttons/ganraj21_2/index.html"
-        ></iframe>
-        <iframe
-          className={classes.container}
-          title="ganraj21_3"
-          src="Buttons/ganraj21_3/index.html"
-        ></iframe>
-
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`ganraj21_1`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        
+        <div> <iframe
           className={classes.container}
           title="AmanPathan"
           src="Buttons/AmanPathan/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`AmanPathan`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="AashviKothari"
           src="Buttons/AashviKothari/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`AashviKothari`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Princeton21_1"
           src="Buttons/Princeton21_1/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Princeton21_1`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="abc"
           src="Buttons/Princeton21/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Princeton21`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="SaurabhBarde-007"
           src="Buttons/SaurabhBarde-007/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`SaurabhBarde-007`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="123shuklaayush"
           src="Buttons/123shuklaayush/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`123shuklaayush`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="123shuklaayush"
           src="Buttons/123shuklaayush_1/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`123shuklaayush`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Arjun-Ingole"
           src="Buttons/Arjun-Ingole/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Arjun-Ingole`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Arjun-Ingole"
           src="Buttons/Arjun-Ingole_1/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Arjun-Ingole_1`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="abc"
           src="Buttons/Spyware007_4/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Spyware007_4`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="abc"
           src="Buttons/Spyware007_6/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Spyware007_6`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="abc"
           src="Buttons/Spyware007_5/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Spyware007_5`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Ayushi8989"
           src="Buttons/Ayushi8989/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Ayushi8989`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Sakshy18"
           src="Buttons/Sakshy18/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`SaurabhBarde-007_2`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
-
           title="25Vaishnavibarge17"
           src="Buttons/25Vaishnavibarge17/index.html"
-        ></iframe>
-        <iframe
+          ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`SaurabhBarde25Vaishnavibarge17`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Aaditya1612"
           src="Buttons/Aaditya1612/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Aaditya1612`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="AbhishekKhairnar"
           src="Buttons/AbhishekKhairnar/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`SaurabhBardeAbhishekKhairnar `, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="GauriBhand023"
           src="Buttons/GauriBhand023/index.html"
-        ></iframe>
-        <iframe
-          className={classes.container}
-
-          title="sumeetk10"
-          src="Buttons/sumeetk10/index.html"
-        ></iframe>
-
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`GauriBhand023`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Sapna127"
           src="Buttons/Sapna127/index.html"
-        ></iframe>
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Sapna127`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
 
-        <iframe
+        <div> <iframe
           className={classes.container}
           title="richesh27"
           src="Buttons/richesh27/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`richesh27`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Bhushan_Kale"
           src="Buttons/Bhushan_Kale/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Bhushan_Kale`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Bhushan_Kale_1"
           src="Buttons/Bhushan_Kale_1/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Bhushan_Kale_1`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="sks8784"
           src="Buttons/sks8784/index.html"
-
-        ></iframe>
-        <iframe
+        ></iframe>  <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`sks8784`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Vaibhav-xt"
           src="Buttons/Vaibhav-xt/index.html"
-        ></iframe>
-        <iframe
+        ></iframe>  
+        <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Vaibhav-xt`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+
+        <div> <iframe
           className={classes.container}
 
           title="geeky-hypertext629"
           src="Buttons/geeky-hypertext629/index.html"
-        ></iframe>
-        <iframe
+        ></iframe> 
+        <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`geeky-hypertext629`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="Shubhammete"
           src="Buttons/Shubhammete/index.html"
-       ></iframe>
-       <iframe
-          className={classes.container}
-          title="PranavKale03"
-          src="Buttons/PranavKale03/index.html"
-       ></iframe>
 
-        <iframe
+        ></iframe> <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`Shubhammete`, `index`, `button`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+
+        <div> <iframe
           className={classes.container}
           title="akpaswan67"
           src="Buttons/akpaswan67/index.html"
-        ></iframe>
+        ></iframe><div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`akpaswan67`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+         </div>
 
-  <iframe
+  <div> <iframe
           className={classes.container}
           title="mundadapurva"
           src="Buttons/mundadapurva/index.html"
-></iframe>
-        <iframe
+></iframe> <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`mundadapurva`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="NMN03"
           src="Buttons/NMN03/index.html"
-        ></iframe>
-        <iframe
+        ></iframe> <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`NMN03`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="FallenEcstasy"
           src="Buttons/FallenEcstasy/index.html"
-        ></iframe>
-        <iframe
+        ></iframe> <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`FallenEcstasy`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div> <iframe
           className={classes.container}
           title="NMN03_1"
           src="Buttons/NMN03_1/index.html"
-        ></iframe>
-
-<iframe
+        ></iframe> <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`NMN03_1`, `index`, `styles`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
+        <div>
+        <iframe
           className={classes.container}
           title="tanishq_khandelwal"
           src="Buttons/tanishq_khandelwal/index.html"
         ></iframe>
-
+        <div className={classes.download}>
+          <button type="submit" onClick ={()=> {download(`tanishq_khandelwal`, `index`, `style`)}} className={classes.btn}>Download</button>
+          </div>
+        </div>
       </div>
+  
     </>
   );
 };
