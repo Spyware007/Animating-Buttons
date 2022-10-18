14aad7c0f488523b6781f33b169be8b5c44f2a93
sumeetk10 <sumeet.k2022@gmail.com>
2022-10-18 11:50:53 +0530

    added new button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -150,7 +150,6 @@ const Main = () => {
           className={classes.container}
           title="sks8784"
           src="Buttons/sks8784/index.html"
-
         ></iframe>
         <iframe
           className={classes.container}
@@ -162,6 +161,11 @@ const Main = () => {
           title="Shubhammete"
           src="Buttons/Shubhammete/index.html"
        ></iframe>
+       <iframe
+          className={classes.container}
+          title="sumeetk10_1"
+          src="Buttons/sumeetk10_1/index.html"
+        ></iframe>
 
       </div>
     </>
