5a499f23e6831eca78d158a55d63a64191e7d80d
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-10-19 04:47:24 +0530

    Download functionality added, Creators name visible & code refactored

---
+++ src/Functions/Download.js
@@ -0,0 +1,24 @@
+function download(name, index, css) {
+  var filesDownload = [];
+  filesDownload.push({
+    path: `Buttons/${name}/${index}.html`,
+    name: `${index}.html`,
+  });
+  filesDownload.push({
+    path: `Buttons/${name}/${css}.css`,
+    name: `${css}.css`,
+  });
+
+  var tempDownloadLink = document.createElement("a");
+  tempDownloadLink.style.display = "none";
+  document.body.appendChild(tempDownloadLink);
+  for (var n = 0; n < filesDownload.length; n++) {
+    var download = filesDownload[n];
+    tempDownloadLink.setAttribute("href", download.path);
+    tempDownloadLink.setAttribute("download", download.name);
+    tempDownloadLink.click();
+  }
+
+  document.body.removeChild(tempDownloadLink);
+}
+export default download;
