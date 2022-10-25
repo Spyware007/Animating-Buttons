1578c310da9e24a1cb2009284302af781447ec83
Sapna Kul <kulsapna2210002@gmail.com>
2022-10-25 16:06:38 +0530

    renamed

--- public/Buttons/Sapna127_5/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.btn').addEventListener('click', (evt) => {
-    evt.target.classList.add('loading')
-  
-    setTimeout(() => {
-      evt.target.classList.remove('loading')
-    }, 3000);
-  })
