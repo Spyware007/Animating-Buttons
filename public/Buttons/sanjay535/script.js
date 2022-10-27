fe2171b5bb4ea244186db7ad1f8ac20732f24eb3
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2022-10-27 06:55:59 +0530

    loader button added from sanjay535

---
+++ public/Buttons/sanjay535/script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+const btnText = document.getElementById('btn-text');
+
+btn.addEventListener('click', () => {
+  document.querySelector('.lds-ring').style.display = 'inline-block';
+  btnText.innerHTML = 'Loading...';
+  setTimeout(() => {
+    document.querySelector('.lds-ring').style.display = 'none';
+    btnText.innerHTML = 'SUBMIT';
+  }, 3000);
+});
+65;
