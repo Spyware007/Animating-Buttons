d4688d15ef6252c07ae65589fcedfaf256f03785
Aditya Tulsiyan <tulsiyanaditya11@gmail.com>
2023-05-27 02:20:19 +0530

    Revert "Update css"
    This reverts commit 2d27b3ef5e502f467ec0bd8a9fa3a410095add14.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,38 +8,42 @@ import sun from "../../assets/sun.png";
 
 const Navbar = ({ modeToggle, modeToggleFunc }) => {
   return (
-    <nav className={classes.navbar}>
-      <a
-        className={classes.link}
-        href="https://gdsc.community.dev/dy-patil-college-of-engineering-pune/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={classes.logo} src={gdsc} alt="gdsc_logo" />
-      </a>
-      <a
-        className={classes.spider}
-        href="https://github.com/spyware007"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={classes.spider_img} src={spider} alt="spider_logo" />
-      </a>
-      <button
-        className={` ${modeToggle ? classes.dark_mode : classes.light_mode}`}
-        onClick={() => modeToggleFunc(!modeToggle)}
-      >
-        <img src={modeToggle ? sun : moon} alt="" />
-      </button>
-      <a
-        className={classes.link}
-        href="https://github.com/Spyware007/Animating-Buttons"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={classes.github} src={github} alt="github_link" />
-      </a>
-    </nav>
+    <>
+      <nav className={classes.navbar}>
+        <a
+          className={classes.link}
+          href="https://gdsc.community.dev/dy-patil-college-of-engineering-pune/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className={classes.logo} src={gdsc} alt="gdsc_logo" />
+        </a>
+        <a
+          className={classes.spider}
+          href="https://github.com/spyware007"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className={classes.spider_img} src={spider} alt="spider_logo" />
+        </a>
+        <button
+          className={`${classes.mode_toggle} ${
+            modeToggle ? classes.dark_mode : classes.light_mode
+          }`}
+          onClick={() => modeToggleFunc(!modeToggle)}
+        >
+          <img src={modeToggle ? sun : moon} alt="" />
+        </button>
+        <a
+          className={classes.link}
+          href="https://github.com/Spyware007/Animating-Buttons"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className={classes.github} src={github} alt="github_link" />
+        </a>
+      </nav>
+    </>
   );
 };
 
