eb41333a1903c02c47d50f58836bec237135bfbc
shobhitjain <jshobhit3020@gmail.com>
2023-05-29 21:23:04 +0530

    Adding Buttons

--- src/Data.js
+++ src/Data.js
@@ -5,7 +5,7 @@ export const Data = [
   "Jacinth-Mahanta",
   "Jacinth-Mahanta2",
   "Jacinth-Mahanta3",
-   "Shreyans2004",
+  "Shreyans2004",
   "abhi03ruchi",
   "Aryan5464",
   "Ayush9951",
@@ -80,7 +80,7 @@ export const Data = [
   "KishorBhagat",
   "KratikJain10",
   "masked-wolf18",
-  "masked-wolf18_2", 
+  "masked-wolf18_2",
   "Killer2OP_2",
   "krishi1211",
   "mundadapurva",
@@ -202,7 +202,7 @@ export const Data = [
   "Naishadha-satya",
   "AdityaLahane-2003_1",
   "mohitparmar1",
-  'Nitin1Sahu_button',
+  "Nitin1Sahu_button",
   "Saurabh-kumar09",
   "khushbumaheshwarii",
   "marshadkhn",
@@ -210,6 +210,6 @@ export const Data = [
   "Explorer003",
   "Pritam991",
   "lmalkam",
-  "priyam212001"
+  "priyam212001",
+  "jshobhit11",
 ];
-
