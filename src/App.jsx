7373903da70fdc0f8a8c1c7c72840002e0cea3b7
Harsh Patil <harshpatil2956@gmail.com>
2023-05-29 13:42:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { Loader } from "./components";
 import MainPage from "./MainPage";
-import { BrowserRouter } from "react-router-dom";
+import ErrorPage from './components/ErrorPage/404Error'
+import { BrowserRouter, Routes,Route } from "react-router-dom";
 
-const App = () => {
+
+const App = ({ modeToggleFunc,modeToggle }) => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
@@ -12,7 +14,13 @@ const App = () => {
     }, 1000);
   }, []);
 
-  return <BrowserRouter>{loading ? <Loader /> : <MainPage />}</BrowserRouter>;
+  return <BrowserRouter>
+
+ <Routes >
+  <Route path="/" element={loading ? <Loader /> : <MainPage modeToggleFunc={modeToggle}  />} />
+  <Route path="*" element={<ErrorPage modeToggleFunc={modeToggle}/>} />
+ </Routes>
+  </BrowserRouter>;
 };
 
 export default App;
