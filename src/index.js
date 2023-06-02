2296d927d22d60754f26e2e6dcaa640d460bc969
Khushi kumari <khushikumarikk79@gmail.com>
2023-06-02 11:45:34 +0530

    more changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Import the CSS file for styling
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const App = () => {
+  const handleSaveChanges = () => {
+    const saveBtn = document.querySelector('.save-btn');
+    saveBtn.innerHTML = "<div class='loader'></div>";
+    setTimeout(() => {
+      saveBtn.innerHTML = "Changes Saved";
+      saveBtn.style.background = "#0000FF";
+      saveBtn.style.color = "#333";
+      saveBtn.style.pointerEvents = "none";
+    }, 2000);
+  };
+
+  return (
+    <div>
+      <button className="save-btn" onClick={handleSaveChanges}>
+        Save Changes
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
