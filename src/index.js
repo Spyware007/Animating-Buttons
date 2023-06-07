f4f96e53320063e60797de301c8cd03e7b0b5066
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-06-07 23:03:59 +0530

    Corrected the alignmendt of button added by abhinav-m22

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </>
 );
