92af96c93fee23fae51cbd281b2028f9e4595494
Akanksha Jha <100708686+Akanksha-202@users.noreply.github.com>
2023-06-18 16:26:28 +0530

    Dev spidey (#1332)
    * Login page backend
    
    * login backend error removed
    
    * Add a button backend connection
    
    * retrival of buttons from backend
    
    * Updated ShowCode Page, fetched data from firestore

--- src/components/common/Card/Card.jsx
+++ src/components/common/Card/Card.jsx
@@ -7,16 +7,27 @@ import LikeButton from "../LikeButton/LikeButton";
 import downloadZip from "../../../Functions/DownloadZip";
 // import ViewsIcon from "../ViewsIcon/ViewsIcon";
 
-const Card = ({ button = "hello_1" }) => {
-  const user = button.split("_")[0];
-  console.log(button);
+const Card = ({ autoid,button }) => {
+  console.log(autoid);
+  const user = button.githubUsername;
+  
   return (
     <>
       <div className={classes.card_container}>
         <iframe
           className={classes.iframe_container}
-          title={button}
-          src={`Buttons/${button}/index.html`}
+          title={autoid}
+          srcDoc={`
+            <html>
+              <head>
+                <style>${button.css}</style>
+              </head>
+              <body>
+                ${button.html}
+                <script>${button.js}</script>
+              </body>
+            </html>
+          `}
           sandbox="allow-scripts"
         ></iframe>
 
@@ -29,12 +40,15 @@ const Card = ({ button = "hello_1" }) => {
                 alt="Spyware007"
               />
             </div>
-            <Link to={`/user/${user}`} className={classes.contributor_name}>
+            <Link
+              to={`/user/${button.githubUsername}`}
+              className={classes.contributor_name}
+            >
               {user}
             </Link>
           </div>
           <div className={classes.btns_container}>
-            <Link to={`/show/${button}`}>
+            <Link to={`/show/${autoid}`}>
               <Button show={true} />
             </Link>
             <Button onClick={() => downloadZip(button)} />
@@ -42,7 +56,7 @@ const Card = ({ button = "hello_1" }) => {
         </div>
         <div className={classes.stats_btn}>
           {/* <ViewsIcon /> */}
-          <LikeButton />
+          <LikeButton  autoid={autoid}/>
         </div>
       </div>
     </>
@@ -50,3 +64,49 @@ const Card = ({ button = "hello_1" }) => {
 };
 
 export default Card;
+
+//prev code
+// const Card = ({ button = "hello_1" }) => {
+//   const user = button.split("_")[0];
+//   console.log(button);
+//   return (
+//     <>
+//       <div className={classes.card_container}>
+//         <iframe
+//           className={classes.iframe_container}
+//           title={button}
+//           src={`Buttons/${button}/index.html`}
+//           sandbox="allow-scripts"
+//         ></iframe>
+
+//         <div className={classes.contributor_info}>
+//           <div className={classes.contributor_data}>
+//             <div className={classes.contributor_img_container}>
+//               <img
+//                 className={classes.contributor_img}
+//                 src="https://avatars.githubusercontent.com/u/76893714?v=4"
+//                 alt="Spyware007"
+//               />
+//             </div>
+//             <Link to={`/user/${user}`} className={classes.contributor_name}>
+//               {user}
+//             </Link>
+//           </div>
+//           <div className={classes.btns_container}>
+//             <Link to={`/show/${button}`}>
+//               <Button show={true} />
+//             </Link>
+//             <Button onClick={() => downloadZip(button)} />
+//           </div>
+//         </div>
+//         <div className={classes.stats_btn}>
+//           {/* <ViewsIcon /> */}
+//           <LikeButton />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Card;
+
