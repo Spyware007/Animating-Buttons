e7930bb226f8294b0716582a642bb5cb8ba84e50
Aditya Tulsiyan <tulsiyanaditya11@gmail.com>
2023-06-21 23:34:44 +0530

    Fix: ShowCode now working
    Update ShowCode.jsx
    
    Minor Changes
    
    Change: minor UI changes to main component
    
    fix

--- src/components/common/Card/Card.jsx
+++ src/components/common/Card/Card.jsx
@@ -7,106 +7,53 @@ import LikeButton from "../LikeButton/LikeButton";
 import downloadZip from "../../../Functions/DownloadZip";
 // import ViewsIcon from "../ViewsIcon/ViewsIcon";
 
-const Card = ({ autoid,button }) => {
+const Card = ({ autoid, button }) => {
   console.log(autoid);
   const user = button.githubUsername;
-  
+
   return (
-    <>
-      <div className={classes.card_container}>
-        <iframe
-          className={classes.iframe_container}
-          title={autoid}
-          srcDoc={`
+    <div className={classes.card_container}>
+      <iframe
+        className={classes.iframe_container}
+        title={autoid}
+        srcDoc={`
             <html>
-              <head>
-                <style>${button.css}</style>
-              </head>
-              <body>
-                ${button.html}
-                <script>${button.js}</script>
-              </body>
+              <head><style>${button.css}</style></head>
+              <body>${button.html}<script>${button.js}</script></body>
             </html>
           `}
-          sandbox="allow-scripts"
-        ></iframe>
+        sandbox="allow-scripts"
+      ></iframe>
 
-        <div className={classes.contributor_info}>
-          <div className={classes.contributor_data}>
-            <div className={classes.contributor_img_container}>
-              <img
-                className={classes.contributor_img}
-                src="https://avatars.githubusercontent.com/u/76893714?v=4"
-                alt="Spyware007"
-              />
-            </div>
-            <Link
-              to={`/user/${button.githubUsername}`}
-              className={classes.contributor_name}
-            >
-              {user}
-            </Link>
+      <div className={classes.contributor_info}>
+        <div className={classes.contributor_data}>
+          <div className={classes.contributor_img_container}>
+            <img
+              className={classes.contributor_img}
+              src="https://avatars.githubusercontent.com/u/76893714?v=4"
+              alt="Spyware007"
+            />
           </div>
-          <div className={classes.btns_container}>
-            <Link to={`/show/${autoid}`}>
-              <Button show={true} />
-            </Link>
-            <Button onClick={() => downloadZip(button)} />
-          </div>
+          <Link
+            to={`/user/${button.githubUsername}`}
+            className={classes.contributor_name}
+          >
+            {user}
+          </Link>
         </div>
-        <div className={classes.stats_btn}>
-          {/* <ViewsIcon /> */}
-          <LikeButton  autoid={autoid}/>
+        <div className={classes.btns_container}>
+          <Link to={`/show/${autoid}`}>
+            <Button show={true} />
+          </Link>
+          <Button onClick={() => downloadZip(button)} />
         </div>
       </div>
-    </>
+      <div className={classes.stats_btn}>
+        {/* <ViewsIcon /> */}
+        <LikeButton autoid={autoid} />
+      </div>
+    </div>
   );
 };
 
 export default Card;
-
-//prev code
-// const Card = ({ button = "hello_1" }) => {
-//   const user = button.split("_")[0];
-//   console.log(button);
-//   return (
-//     <>
-//       <div className={classes.card_container}>
-//         <iframe
-//           className={classes.iframe_container}
-//           title={button}
-//           src={`Buttons/${button}/index.html`}
-//           sandbox="allow-scripts"
-//         ></iframe>
-
-//         <div className={classes.contributor_info}>
-//           <div className={classes.contributor_data}>
-//             <div className={classes.contributor_img_container}>
-//               <img
-//                 className={classes.contributor_img}
-//                 src="https://avatars.githubusercontent.com/u/76893714?v=4"
-//                 alt="Spyware007"
-//               />
-//             </div>
-//             <Link to={`/user/${user}`} className={classes.contributor_name}>
-//               {user}
-//             </Link>
-//           </div>
-//           <div className={classes.btns_container}>
-//             <Link to={`/show/${button}`}>
-//               <Button show={true} />
-//             </Link>
-//             <Button onClick={() => downloadZip(button)} />
-//           </div>
-//         </div>
-//         <div className={classes.stats_btn}>
-//           {/* <ViewsIcon /> */}
-//           <LikeButton />
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Card;
-
