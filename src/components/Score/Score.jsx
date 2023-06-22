128c3117e29f6e56877002020a48d4cf5072f3c4
SouravvMishra <souravvmishra@gmail.com>
2023-06-22 22:06:49 +0530

    Fixed Image Not displaying

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import classes from "./Score.module.css";
 import { Link } from "react-router-dom";
-import { Data } from "../../Data";
 import memoji from "../../assets/memoji.png";
 import { collection, getDocs, } from 'firebase/firestore'
 import { db } from "../../firebase/auth"
@@ -9,15 +8,30 @@ import { db } from "../../firebase/auth"
 const Score = () => {
 
   const [buttons, setButtons] = useState([])
+  const [users, setusers] = useState([])
+  
   useEffect(() => {
     fetchButtons();
   }, [])
 
   async function fetchButtons() {
     const btnsCol = collection(db, "buttons");
+    const usersCol = collection(db, "users");
+
     const btnsSnapshot = await getDocs(btnsCol);
+    const usersSnapshot = await getDocs(usersCol);
+
     const btnsList = btnsSnapshot.docs.map(doc => doc.data());
+    const usersList = usersSnapshot.docs.map(doc => doc.data());
+
     setButtons(btnsList);
+    setusers(usersList);
+  }
+
+  function getProfilePicture(username) {
+    // find the user object that matches the input parameter
+    let user = users.find(user => user.username.toLowerCase() === username.toLowerCase());
+    return user ? (user.profilePictureUrl) : memoji;
   }
 
 
@@ -43,7 +57,7 @@ const Score = () => {
           <Link to={`/user/${name}`} key={index} className={classes.item}>
             <span className=
               {classes.rank}>{rank++ + "."}</span>
-            <img src={memoji} alt="Profile" className={classes.profileImage} />
+            <img src={getProfilePicture(name)} alt="Profile" className={classes.profileImage} />
             <div className={classes.details}>
               <div className={classes.name}>{name}</div>
               <div className={classes.count}>{count} buttons added</div>
