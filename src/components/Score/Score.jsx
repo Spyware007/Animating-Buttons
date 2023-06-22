85fe40c15e08e77fbfc539590e4b322b6602c387
SouravvMishra <souravvmishra@gmail.com>
2023-06-22 15:23:58 +0530

    Leader Board Working Fine for number of buttons

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Score.module.css";
 import { Link } from "react-router-dom";
 import { Data } from "../../Data";
 import memoji from "../../assets/memoji.png";
+import { collection, getDocs, } from 'firebase/firestore'
+import { db } from "../../firebase/auth"
 
 const Score = () => {
-  const cleanedArray = Data.map((item) => item.replace(/_\d+$/, ""));
+
+  const [buttons, setButtons] = useState([])
+  useEffect(() => {
+    fetchButtons();
+  }, [])
+
+  async function fetchButtons() {
+    const btnsCol = collection(db, "buttons");
+    const btnsSnapshot = await getDocs(btnsCol);
+    const btnsList = btnsSnapshot.docs.map(doc => doc.data());
+    setButtons(btnsList);
+  }
+
+
+  // const cleanedArray = Data.map((item) => item.replace(/_\d+$/, ""));
+  const cleanedArray = buttons.map(btn => btn.githubUsername)
   const uniqueValues = new Set(cleanedArray);
 
   const hashmap = {};
@@ -16,13 +33,16 @@ const Score = () => {
   });
 
   const sortedArray = Object.entries(hashmap).sort((a, b) => b[1] - a[1]);
+  let rank = 1;
 
   return (
     <>
       <h1 className={classes.leaderboard_text}>LEADERBOARD</h1>
       <div className={classes.leaderboard}>
         {sortedArray.map(([name, count], index) => (
-          <Link to={`/user/${name}`} key={name} className={classes.item}>
+          <Link to={`/user/${name}`} key={index} className={classes.item}>
+            <span className=
+              {classes.rank}>{rank++ + "."}</span>
             <img src={memoji} alt="Profile" className={classes.profileImage} />
             <div className={classes.details}>
               <div className={classes.name}>{name}</div>
@@ -30,7 +50,7 @@ const Score = () => {
             </div>
           </Link>
         ))}
-      </div>
+      </div >
     </>
   );
 };
