0f16e4e3a1dbda4bb0bf7774fd64fac9395c5f37
SouravvMishra <souravvmishra@gmail.com>
2023-06-24 20:21:43 +0530

    Two Leaderboard Showing

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from "react";
 import classes from "./Score.module.css";
 import { Link } from "react-router-dom";
-import { Data } from "../../Data";
 import memoji from "../../assets/memoji.png";
 import { collection, getDocs, } from 'firebase/firestore'
 import { db } from "../../firebase/auth"
+import Loader from "../common/Loader/Loader";
 
 const Score = () => {
 
   const [buttons, setButtons] = useState([])
+  const [users, setUsers] = useState([])
+
   useEffect(() => {
     fetchButtons();
   }, [])
 
   async function fetchButtons() {
     const btnsCol = collection(db, "buttons");
+    const usersCol = collection(db, "users");
+
     const btnsSnapshot = await getDocs(btnsCol);
+    const usersSnapshot = await getDocs(usersCol);
+
     const btnsList = btnsSnapshot.docs.map(doc => doc.data());
+    const usersList = usersSnapshot.docs.map(doc => doc.data());
+
+
     setButtons(btnsList);
+    setUsers(usersList);
+    buttons.sort((a, b) => b.likeCounter - a.likeCounter)
+  }
+
+  function getProfilePicture(username) {
+    // find the user object that matches the input parameter
+    let user = users.find(user => user.username.toLowerCase() === username.toLowerCase());
+    return user ? (user.profilePictureUrl) : memoji;
   }
 
 
@@ -27,32 +44,70 @@ const Score = () => {
 
   const hashmap = {};
 
+
   uniqueValues.forEach((item) => {
     const count = cleanedArray.filter((value) => value === item).length;
     hashmap[item] = count;
   });
 
-  const sortedArray = Object.entries(hashmap).sort((a, b) => b[1] - a[1]);
-  let rank = 1;
+  var totalLikes = buttons.reduce(function (acc, cur) {
+    acc[cur.githubUsername] = (acc[cur.githubUsername] || 0) + cur.likeCounter;
+    return acc;
+  }, {});
+
+  const sortedUsers = Object.entries(hashmap).sort((a, b) => b[1] - a[1]);
+  const sortedLikes = Object.entries(totalLikes).sort((a, b) => b[1] - a[1]);
+
+
+
+  let rankNumber = 1;
+  let rankLikes = 1;
+
+  if (buttons.length === 0 ||!users.length === 0){
+    return <div><Loader/></div>;
+  }
 
   return (
     <>
       <h1 className={classes.leaderboard_text}>LEADERBOARD</h1>
-      <div className={classes.leaderboard}>
-        {sortedArray.map(([name, count], index) => (
-          <Link to={`/user/${name}`} key={index} className={classes.item}>
-            <span className=
-              {classes.rank}>{rank++ + "."}</span>
-            <img src={memoji} alt="Profile" className={classes.profileImage} />
-            <div className={classes.details}>
-              <div className={classes.name}>{name}</div>
-              <div className={classes.count}>{count} buttons added</div>
-            </div>
-          </Link>
-        ))}
+
+      <div className={classes.list}>
+
+        <div className={classes.leaderboard}>
+          <h2 className={classes.category}> Number Of Buttons</h2>
+
+          
+          {sortedUsers.map(([name, count], index) => (
+            <Link to={`/user/${name}`} key={index} className={classes.item}>
+              <span className=
+                {classes.rank}>{rankNumber++ + "."}</span>
+              <img src={getProfilePicture(name)} alt="Profile" className={classes.profileImage} />
+              <div className={classes.details}>
+                <div className={classes.name}>{name}</div>
+                <div className={classes.count}>{count} buttons added</div>
+              </div>
+            </Link>
+          ))}
+        </div >
+
+        <div className={classes.leaderboard}>
+          <h2 className={classes.category}> Number Of Likes</h2>
+          {sortedLikes.map(([name, count], index) => (
+            <Link to={`/user/${name}`} key={index} className={classes.item}>
+              <span className=
+                {classes.rank}>{rankLikes++ + "."}</span>
+              <img src={getProfilePicture(name)} alt="Profile" className={classes.profileImage} />
+              <div className={classes.details}>
+                <div className={classes.name}>{name}</div>
+                <div className={classes.count}>Total Likes : {count} </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div >
     </>
   );
 };
 
 export default Score;
+
