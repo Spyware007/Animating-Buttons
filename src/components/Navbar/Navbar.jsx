0b4ca44b763ed6cd5adf9d682360479d6bd5e7c0
Rishu <rrishu561@gmail.com>
2023-06-27 19:01:07 +0530

    add style: gradient create button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -91,8 +91,8 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                width="24"
-                height="24"
+                width="30"
+                height="30"
               >
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path
