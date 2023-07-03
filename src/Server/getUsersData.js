c3f1c94792f7d362d2d9c57782cfbd30d51ba31b
Aditya Tulsiyan <72987111+adity2911@users.noreply.github.com>
2023-07-03 19:21:10 +0530

    Added Cache function to store data after fetching it from the database (#1701)
    * Added cache memory function
    
    Now the data, on being fetched is first saved to the local storage. After this, the data from the local storage is provided in case of any requests.
    
    * Update: Cards design

--- src/Server/getUsersData.js
+++ src/Server/getUsersData.js
@@ -1,10 +1,18 @@
 import { db } from "../firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
 
+const USERS_CACHE_KEY = "usersData";
+
 export async function getUsersData() {
+  const cachedData = localStorage.getItem(USERS_CACHE_KEY);
+  if (cachedData) {
+    return JSON.parse(cachedData);
+  }
+
   const usersCollectionRef = collection(db, "users");
   const data = await getDocs(usersCollectionRef);
 
   const finalData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  localStorage.setItem(USERS_CACHE_KEY, JSON.stringify(finalData));
   return finalData;
 }
