9bf9d27e3b8fbc040b24e50e709118e41252fd81
Sourav Mishra <89072785+Souravvmishra@users.noreply.github.com>
2023-07-03 22:48:10 +0530

    Fixed The Bug In Navbar, Also Made Some Changes To Fix The White Screen (hopefully fixed) (#1707)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -142,51 +142,54 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
           </button>
         </ul>
         <div className={classes.button_container}>
-          <NavLink className={classes.list_item_link} to="/add">
-            <button className={classes.add}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="30"
-                height="30"
-              >
-                <path fill="none" d="M0 0h24v24H0z"></path>
-                <path
-                  fill="currentColor"
-                  d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"
-                ></path>
-              </svg>
-              <span className={classes.addbtn}>Create</span>
-            </button>
-          </NavLink>
 
           {userImage && userEmail ? (
-            <div className={classes.loggedIn}>
-              <NavLink
-                className={classes.list_item_link}
-                to={`/user/${username}`}
-              >
-                <button className={classes.github}>
-                  <div className={classes.image_container}>
-                    <img
-                      className={classes.image}
-                      src={userImage}
-                      alt="Creator"
-                    />
-                  </div>
-                  <span className={classes.username}>My Profile</span>
+
+              <div className={classes.loggedIn}>
+                <NavLink className={classes.list_item_link} to="/add">
+                  <button className={classes.add}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="30"
+                      height="30"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path
+                        fill="currentColor"
+                        d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"
+                      ></path>
+                    </svg>
+                    <span className={classes.addbtn}>Create</span>
+                  </button>
+                </NavLink>
+                <NavLink
+                  className={classes.list_item_link}
+                  to={`/user/${username}`}
+                >
+                  <button className={classes.github}>
+                    <div className={classes.image_container}>
+                      <img
+                        className={classes.image}
+                        src={userImage}
+                        alt="Creator"
+                      />
+                    </div>
+                    <span className={classes.username}>My Profile</span>
+                  </button>
+                </NavLink>
+                <button className={classes.logOut} onClick={handleLogout}>
+                  <img
+                  style={{
+                    width:'1rem', height:'1rem'
+                  }}
+                    src={logout}
+                    alt="Log Out"
+                    height={"20px"}
+                    width={"40px"}
+                  />
                 </button>
-              </NavLink>
-              <button className={classes.logOut} onClick={handleLogout}>
-                <img
-                  src={logout}
-                  alt="Log Out"
-                  srcset=""
-                  height={"20px"}
-                  width={"40px"}
-                />
-              </button>
-            </div>
+              </div>
           ) : (
             <div className={classes.list_item_link} onClick={handleGitHubLogin}>
               <button className={classes.github}>
