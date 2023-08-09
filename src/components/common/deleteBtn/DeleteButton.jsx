1273b5830bf0ea07dcd9610b39ed4a130983a503
SouravvMishra <souravvmishra@gmail.com>
2023-08-09 13:33:53 +0530

    delete Button Was present even on user loggedout, added dyna,mic delete btn

--- src/components/common/deleteBtn/DeleteButton.jsx
+++ src/components/common/deleteBtn/DeleteButton.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import classes from "./DeleteButton.module.css";
+import { useAuthState } from "react-firebase-hooks/auth"; // Import the appropriate hook
+import { auth } from "../../../firebase/auth";
 
 const DeleteButton = ({ modeToggle, handleDelete }) => {
+  const [user, loading, error] = useAuthState(auth); // Use the useAuthState hook
+  if (loading) {
+    return null; // Return nothing while authentication state is loading
+  }
+
+  if (error) {
+    // Handle authentication error if needed
+    console.error("Authentication error:", error);
+    return null;
+  }
+
+  if (!user) {
+    return null; // Return nothing if the user is not logged in
+  }
+
   return (
     <div
       onClick={handleDelete}
