3a98cfa928f80e902f582ec25f7f5d00fc9f43d4
Spyware007 <omgawandeofficial9834899149@gmail.com>
2023-08-22 20:02:16 +0530

    minor fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,32 +8,27 @@ import logout from "../../assets/logout-svgrepo-com.svg";
 import { handleLogout, handleGitHubLogin } from "./loginHelper";
 import { Toaster } from "react-hot-toast";
 
-
 // images
 import moon from "../../assets/moon.webp";
 import sun from "../../assets/sun.webp";
 
 const Navbar = ({ modeToggle, modeToggleFunc }) => {
-
   const [user, setUser] = useState({ username: "", profilePictureUrl: "" });
   const [githubBio, setGithubBio] = useState("");
   const [githubSocialAccounts, setGithubSocialAccounts] = useState([]);
-  const [navbarVisible, setNavbarVisible] = useState(true)
-
-
+  const [navbarVisible, setNavbarVisible] = useState(true);
 
   const location = useLocation();
-
 
   const handleResize = () => {
     setNavbarVisible(window.innerWidth > 768);
     if (window.innerWidth > 768) {
-      setNavbarVisible(true)
+      setNavbarVisible(true);
     }
   };
   useEffect(() => {
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     const auth = getAuth();
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -43,11 +38,9 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
         localStorage.setItem("username", user?.reloadUserInfo?.screenName);
         localStorage.setItem("email", user.email);
         localStorage.setItem("userImage", user.photoURL);
-        
-      }
-      else {
+      } else {
         // handleLogout(setUser);
-        setUser(null)
+        setUser(null);
         localStorage.clear();
       }
     });
@@ -59,53 +52,86 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
   return (
     <>
       <nav
-        className={`${classes.navbar} ${!modeToggle ? classes["navbar-light"] : classes["navbar-dark"]
-          }`}
+        className={`${classes.navbar} ${
+          !modeToggle ? classes["navbar-light"] : classes["navbar-dark"]
+        }`}
       >
-        <span className={classes.hamburger} onClick={() => setNavbarVisible(true)}>☰</span>
-        {navbarVisible && (<div><span className={classes.cross} onClick={() => window.innerWidth < 768 && setNavbarVisible(false)}>✖</span>
-          <ul className={classes.navlist}>
-            <li className={`${classes.list_item} ${location.pathname === '/' && classes.active} `} onClick={() => window.innerWidth < 768 && setNavbarVisible(false)}>
-              <NavLink className={classes.list_item_link} to="/">
-                Home
-              </NavLink>
-            </li>
-            <li className={`${classes.list_item} ${location.pathname === '/explore' && classes.active} `} onClick={() => window.innerWidth < 768 && setNavbarVisible(false)}>
-              <NavLink className={classes.list_item_link} to="/explore">
-                Explore
-              </NavLink>
-            </li>
-            <li className={`${classes.list_item} ${location.pathname === '/about' && classes.active} `} onClick={() => window.innerWidth < 768 && setNavbarVisible(false)}>
-              <NavLink className={classes.list_item_link} to="/about">
-                About
-              </NavLink>
-            </li>
-            {/* <li className={classes.list_item}>
+        <span
+          className={classes.hamburger}
+          onClick={() => setNavbarVisible(true)}
+        >
+          ☰
+        </span>
+        {navbarVisible && (
+          <div>
+            <span
+              className={classes.cross}
+              onClick={() => window.innerWidth < 768 && setNavbarVisible(false)}
+            >
+              ✖
+            </span>
+            <ul className={classes.navlist}>
+              <li
+                className={`${classes.list_item} ${
+                  location.pathname === "/" && classes.active
+                } `}
+                onClick={() =>
+                  window.innerWidth < 768 && setNavbarVisible(false)
+                }
+              >
+                <NavLink className={classes.list_item_link} to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li
+                className={`${classes.list_item} ${
+                  location.pathname === "/explore" && classes.active
+                } `}
+                onClick={() =>
+                  window.innerWidth < 768 && setNavbarVisible(false)
+                }
+              >
+                <NavLink className={classes.list_item_link} to="/explore">
+                  Explore
+                </NavLink>
+              </li>
+              <li
+                className={`${classes.list_item} ${
+                  location.pathname === "/about" && classes.active
+                } `}
+                onClick={() =>
+                  window.innerWidth < 768 && setNavbarVisible(false)
+                }
+              >
+                <NavLink className={classes.list_item_link} to="/about">
+                  About
+                </NavLink>
+              </li>
+              {/* <li className={classes.list_item}>
             <Link className={classes.list_item_link} to={"/leaderboard"}>
               Creators
             </Link>
           </li> */}
-            <a
-              href="https://github.com/Spyware007/Animating-Buttons"
-              target="__blank"
-              className={classes.image_container_mobile}
-            >
-              <BsGithub className={classes.image} />
-            </a>
-            <button
-              className={`${classes.mode_toggle} ${modeToggle ? classes.dark_mode : classes.light_mode
+              <a
+                href="https://github.com/Spyware007/Animating-Buttons"
+                target="__blank"
+                className={classes.image_container_mobile}
+              >
+                <BsGithub className={classes.image} />
+              </a>
+              <button
+                className={`${classes.mode_toggle} ${
+                  modeToggle ? classes.dark_mode : classes.light_mode
                 }`}
-              onClick={() => modeToggleFunc(!modeToggle)}
-            >
-              <img src={modeToggle ? sun : moon} alt="" loading="lazy" />
-            </button>
-          </ul>
-        </div>
+                onClick={() => modeToggleFunc(!modeToggle)}
+              >
+                <img src={modeToggle ? sun : moon} alt="" loading="lazy" />
+              </button>
+            </ul>
+          </div>
         )}
         <div className={classes.button_container}>
-
           {user ? (
-
             <div className={classes.loggedIn}>
               <NavLink className={classes.list_item_link} to="/add">
                 <button className={classes.add} title="Create New Button">
@@ -126,13 +152,21 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
               </NavLink>
               <NavLink
                 className={classes.list_item_link}
-                to={`/user/${user?.reloadUserInfo?.screenName ? user.reloadUserInfo.screenName : localStorage.getItem('username')}`}
+                to={`/user/${
+                  user?.reloadUserInfo?.screenName
+                    ? user.reloadUserInfo.screenName
+                    : localStorage.getItem("username")
+                }`}
               >
                 <button className={classes.github}>
                   <div className={classes.image_container}>
                     <img
                       className={classes.image}
-                      src={user.photoURL ? user.photoURL : localStorage.getItem('userImage')}
+                      src={
+                        user.photoURL
+                          ? user.photoURL
+                          : localStorage.getItem("userImage")
+                      }
                       alt="Creator"
                       loading="lazy"
                     />
@@ -140,10 +174,15 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
                   <span className={classes.username}>My Profile</span>
                 </button>
               </NavLink>
-              <button className={classes.logOut} onClick={() => handleLogout(setUser)}>
+              <button
+                className={classes.logOut}
+                onClick={() => handleLogout(setUser)}
+              >
                 <img
                   style={{
-                    width: '1rem', height: '1rem'
+                    width: "1rem",
+                    height: "1rem",
+                    color: "white",
                   }}
                   src={logout}
                   alt="Log Out"
@@ -154,7 +193,16 @@ const Navbar = ({ modeToggle, modeToggleFunc }) => {
               </button>
             </div>
           ) : (
-            <div className={`${classes.list_item_link}  ${classes.signInBtn}`} onClick={() => handleGitHubLogin(setUser, setGithubBio, setGithubSocialAccounts)}>
+            <div
+              className={`${classes.list_item_link}  ${classes.signInBtn}`}
+              onClick={() =>
+                handleGitHubLogin(
+                  setUser,
+                  setGithubBio,
+                  setGithubSocialAccounts
+                )
+              }
+            >
               <button className={classes.github}>
                 <div className={classes.image_container}>
                   <BsGithub className={classes.image} />
